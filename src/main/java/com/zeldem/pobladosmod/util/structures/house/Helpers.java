53c28872c00d885cfad4b09d3d884f997ec3670c
Danny <degarzonm@unla.edu.co>
2020-09-06 23:12:14 -0500

    transformacion lineal
    se adaptó una función global para permitir el posicionamiento relativo al bloque puesto y la orientación del jugador/agente

--- src/main/java/com/zeldem/pobladosmod/util/structures/house/Helpers.java
+++ src/main/java/com/zeldem/pobladosmod/util/structures/house/Helpers.java
@@ -1,11 +1,60 @@
 package com.zeldem.pobladosmod.util.structures.house;
 import java.util.concurrent.ThreadLocalRandom;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
 
 public class Helpers {
 
+	
 	public static int rand(int min, int max) { 
 		return ThreadLocalRandom.current().nextInt(min, max + 1);
 	}
+	
+	
+
+	public static boolean setBlock_relative(World mundo, BlockPos e,Direction f, int i, int j, int k, BlockState block) {
+
+		int x,y,z;
+		switch(f) {
+			case 	SOUTH:
+			default:{
+				x=i;
+				y=j;
+				z=k;
+				
+				break;
+			}
+			case 	NORTH:{
+				x=-i;
+				y=j;
+				z=-k;
+				block=block.rotate(Rotation.CLOCKWISE_180);
+				break;
+			}
+			case EAST:{
+				x=k;
+				y=j;
+				z=-i;
+				block=block.rotate(Rotation.COUNTERCLOCKWISE_90);
+				break;
+			}
+			case WEST:{
+				x=-k;
+				y=j;
+				z=i;
+				block=block.rotate(Rotation.CLOCKWISE_90);
+				break;
+			}
+		}
+
+		mundo.setBlockState(new BlockPos(e.getX()+x,e.getY()+y,e.getZ()+z), block, 3);
+		return true;
+	}
+	
 	
 }
--- src/main/java/com/zeldem/pobladosmod/util/structures/house/House.java
+++ src/main/java/com/zeldem/pobladosmod/util/structures/house/House.java
@@ -3,6 +3,7 @@ package com.zeldem.pobladosmod.util.structures.house;
 import com.zeldem.pobladosmod.PobladosMod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.SlabBlock;
@@ -23,160 +24,23 @@ public class House {
 	private int nivel;//define la cantidad de espacios que la casa tendrá
 	private Direction orientacion;
 	
+	
 	public House(World mundo, BlockPos entrada ,int level, Direction orientacion) {
 		this.mundo=mundo;
 		x_offset=z_offset=0;
 		this.entrada=entrada;
 		this.nivel=level>=0?level:3;
 		this.orientacion=orientacion;
-		
 	}
 	
 	public void buildHouse() {
-		Sala principal = new Sala(mundo,entrada	,Helpers.rand(2, 6)	
-												,Helpers.rand(1, 3)		
+		if(!mundo.isRemote) {
+		Sala principal = new Sala(mundo,entrada	,orientacion
+												,Helpers.rand(3, 6)
+												,Helpers.rand(2, 5)
 												,Helpers.rand(1, 4)
-												,Helpers.rand(3, 5));
-		
-	}
-	
-	
-	
-	class Sala{
-		World mundo;
-		BlockPos e;	//puerta
-		int delta_i, delta_d;
-		int frente;
-		int altura;
-		
-		public Sala(World mundo, BlockPos entrada,int frente, int d_i, int d_d, int altura) {
-			this.mundo=mundo;
-			delta_i=d_i>=1?d_i:1;
-			delta_d=d_d>=2?d_d:2;
-			this.frente=frente>=2?frente:2;
-			this.altura=altura>=2?altura:2;
-			this.e=entrada;
-			
-			buildParedes(altura);
-			buildPuertas(Blocks.OAK_DOOR);
-			
-			buildTecho();
+												,Helpers.rand(4, 7));
 		}
-			
-		
-		
-		
-		
-		
-		private void buildParedes(int altura) {
-			int a,b,c,d,r,s,t,u;
-			
-			switch(orientacion) {
-				case SOUTH:
-					 default:{
-					a=u=delta_d;
-					b=t=-delta_i;
-					c=s=frente;
-					d=r=0;
-					
-					break;
-				}
-				case NORTH:{
-					a=u=delta_d;
-					b=t=-delta_i;
-					c=r=0;
-					d=s=-frente;
-					break;
-				}
-				case WEST:{
-					a=u=0;
-					b=t=-frente;
-					c=r=delta_i;
-					d=s=-delta_d;
-					break;
-				}
-				case EAST:{
-					a=t=frente;
-					b=u=0;
-					c=r=delta_i;
-					d=s=-delta_d;
-					break;
-				}
-				
-			}
-			
-			
-			if(!mundo.isRemote) {
-				for (int h=1;h<=altura;h++) {
-					for(int n=e.getX()+b;n<=e.getX()+a;n++) {	
-						mundo.setBlockState(new BlockPos(n,e.getY()+h,e.getZ()+r)		, Blocks.COBBLESTONE.getDefaultState(), 3);
-						mundo.setBlockState(new BlockPos(n,e.getY()+h,e.getZ()+s)		, Blocks.COBBLESTONE.getDefaultState(), 3);
-					}
-					
-					for(int m=e.getZ()+d;m<=e.getZ()+c;m++) {
-						mundo.setBlockState(new BlockPos(e.getX()+t,e.getY()+h,m)		, Blocks.COBBLESTONE.getDefaultState(), 3);
-						mundo.setBlockState(new BlockPos(e.getX()+u,e.getY()+h,m)		, Blocks.COBBLESTONE.getDefaultState(), 3);
-					}
-					
-					
-				}
-			}
-		}
-		
-		public void buildPuertas(Block puerta){
-			mundo.setBlockState(e.up(), puerta.getDefaultState().with(DoorBlock.FACING, orientacion), 3);
-            mundo.setBlockState(e.add(0, 2, 0), puerta.getDefaultState().with(DoorBlock.HALF, DoubleBlockHalf.UPPER).with(DoorBlock.FACING, orientacion), 3);
-			
-		}
-		
-		public boolean buildTecho() {
-			
-			int a,b,c,d;
-			
-			switch(orientacion) {
-			case SOUTH:
-				 default:{
-					 a=-delta_i;
-					 b=delta_d;
-					 c=0;
-					 d=frente;
-				break;
-			}
-			case NORTH:{
-				a=-delta_i;
-				 b=delta_d;
-				 c=-frente;
-				 d=0;
-				break;
-			}
-			case EAST:{
-				a=0;
-				 b=frente;
-				 c=-delta_d;
-				 d=delta_i;
-				break;
-			}
-			case WEST:{
-				a=-frente;
-				b=0;
-				c=-delta_d;
-				d=delta_i;
-				break;
-			}
-			
-		}
-			if(!mundo.isRemote) {
-				for(int i=e.getX()+a;i<=e.getX()+b;i++) {
-					for(int j=e.getZ()+c;j<=e.getZ()+d;j++) {
-						mundo.setBlockState(new BlockPos(i,e.getY()+altura,j), Blocks.ACACIA_SLAB.getDefaultState().with(SlabBlock.TYPE, SlabType.TOP), 3);
-					}
-				}
-			}
-			return true;
-			
-			
-		}
-		
 	}
 	
 }
